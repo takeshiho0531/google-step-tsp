c851e94779339b9e9bc5e54039295dc8141cc9de
Akiho Kawada <kawadatakeshiho@gmail.com>
2023-06-20 16:42:56 +0900

    wip: 一旦2optとsa分割した

--- solver_sa.py
+++ solver_sa.py
@@ -18,12 +18,59 @@ def rand_int(a: int, b: int) -> int:
     """
     return random.randint(a,b)
 
-def get_score(path,cities):
+def get_distance(path,cities):
     N=len(cities)
     score=0
     for i in range(N):
         score+=distance(cities[path[i]], cities[path[i+1]])
     return score
+
+def two_opt(path, N):
+    print("path",path)
+
+    left=rand_int(2,N)
+    right=rand_int(2, N)
+    if (left>right):
+        left, right=right, left
+    updated_path=path[:left]+path[left:right][::-1]+path[right:]
+    print("updated_path",updated_path)
+
+    return path, updated_path
+
+
+def reverse_segment_if_better(tour, i, j, k):
+    """If reversing tour[i:j] would make the tour shorter, then do it."""
+    # Given tour [...A-B...C-D...E-F...]
+    A, B, C, D, E, F = tour[i-1], tour[i], tour[j-1], tour[j], tour[k-1], tour[k % len(tour)]
+    d0 = distance(A, B) + distance(C, D) + distance(E, F)
+    d1 = distance(A, C) + distance(B, D) + distance(E, F)
+    d2 = distance(A, B) + distance(C, E) + distance(D, F)
+    d3 = distance(A, D) + distance(E, B) + distance(C, F)
+    d4 = distance(F, B) + distance(C, D) + distance(E, A)
+
+    if d0 > d1:
+        tour[i:j] = reversed(tour[i:j])
+        return -d0 + d1
+    elif d0 > d2:
+        tour[j:k] = reversed(tour[j:k])
+        return -d0 + d2
+    elif d0 > d4:
+        tour[i:k] = reversed(tour[i:k])
+        return -d0 + d4
+    elif d0 > d3:
+        tmp = tour[j:k] + tour[i:j]
+        tour[i:k] = tmp
+        return -d0 + d3
+    return 0
+
+
+def three_opt(path, N):
+    left, intermediate, right =hoge()
+
+
+
+
+
 
 def solve(cities, trial, initial_path):
     N = len(cities)
@@ -31,18 +78,11 @@ def solve(cities, trial, initial_path):
     path.append(0)
 
     for i in range(trial):
-        print("path",path)
-        score=get_score(path,cities)
-        left=rand_int(2,N)
-        right=rand_int(2, N)
-        if (left>right):
-            left, right=right, left
-        updated_path=path[:left]+path[left:right][::-1]+path[right:]
-        print("updated_path",updated_path)
-        updated_score=get_score(updated_path, cities)
-
+        path, updated_path=two_opt(path, N)
+        distance=get_distance(path,cities)
+        updated_distance=get_distance(updated_path, cities)
         temperature=30-28*i/trial
-        probability=math.exp(min(0, (updated_score-score)/temperature))
+        probability=math.exp(min(0, (distance-updated_distance)/temperature))
 
         random_prob=random.random()
         if random_prob<probability:
